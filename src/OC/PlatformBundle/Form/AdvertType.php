4b26a37a2b7fe1a389bf2d1dab529eb3af8c328e
Loïc Gallay <lgallay@orange.fr>
2017-11-06 17:05:03 +0100

    Fully fonctionnal backend

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 // use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,7 +25,7 @@ class AdvertType extends AbstractType
     {
         // $pattern = 'D%';
 
-        $builder->add('date',        DateType::class)
+        $builder->add('date',        DateTimeType::class)
                 ->add('title',       TextType::class)
                 ->add('author',      TextType::class)
                 ->add('email',       EmailType::class)
